2284905bd29e33a3b3b06714f35f371479741ebc
Christopher Pace <Christopher_pace@ymail.com>
2018-10-19 10:17:08 -0500

    exercise 1

---
+++ ex1.js
@@ -0,0 +1,16 @@
+function Main(input1, input2){
+    // input1 = parseInt(prompt("Please input your first number: "));
+    // input2 = parseInt(prompt("Please input your second number: "));
+    if (input2<input1){
+        console.log("-1")
+    }
+    else if (input1===input2){
+        console.log("Those numbers are the same!")
+    }
+    else if (input2>input1){
+        for (x=input1+1; x<input2; x++){
+         console.log(x)
+        }
+    }
+}
+Main(1, 6);
